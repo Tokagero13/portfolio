6ea73aab111346a4016439e61ce14ea61d839259
Sabit <sabitshangerey@gmail.com>
2024-06-03 18:43:15 +0500

    Portfolio

---
+++ projects/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='projects/images/')
+
+    def __str__(self):
+        return self.title
+
+class Repository(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='repositories')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    code = models.TextField()
+    
+    def __str__(self):
+        return self.title
